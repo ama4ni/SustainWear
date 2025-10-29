869da0aad51475c9c3aa9a53a86f447b2ab19390
edamame91 <118413315+edamame91@users.noreply.github.com>
2025-10-29 16:28:30 +0000

    React/node/express + axiom initial setup

--- app.js
+++ app.js
@@ -1,51 +1,23 @@
-require('dotenv').config();
 
-const express = require('express');
-const session = require('express-session');
-const flash = require('connect-flash');
-const path = require('path');
+import axios from 'axios';
+import './App.css';
 
-const app = express();
+//data will be the string we send from our server
+const apiCall = () => {
+  axios.get('http://localhost:8080').then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data)
+  })
+}
 
-// View engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-	secret: process.env.SESSION_SECRET || 'secretkey123',
-	resave: false,
-	saveUninitialized: true,
-}));
-app.use(flash());
+        <button onClick={apiCall}>Make API Call</button>
 
-app.get('/', (req, res) => {
-	return res.redirect('/login');
-});
-
-app.get('/login', (req, res) => {
-	const success = req.flash('success') || [];
-	const message = req.flash('message') || [];
-	const errors = req.flash('errors') || [];
-	const signupLink = '/signup';
-	res.render('login', { success, message, errors, signupLink });
-});
-
-app.get('/signup', (req, res) => {
-	const message = req.flash('message')?.[0] || '';
-	res.render('signup', { message, formData: {}, csrfToken: '' });
-});
-
-app.get('/forgotpassword', (req, res) => {
-	const success = req.flash('success') || [];
-	const message = req.flash('message')?.[0] || '';
-	res.render('forgotpassword', { success, message });
-});
-
-const PORT = process.env.PORT || 3002;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
+      </header>
+    </div>
+  );
+}
